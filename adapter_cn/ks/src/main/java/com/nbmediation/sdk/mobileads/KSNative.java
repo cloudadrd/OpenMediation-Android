f0ef5602b273a9b37d4c21e754ba4c555a890e63
Mirinda <15967863@qq.com>
2020-11-10 17:27:06 +0800

    完成代码

--- adapter_cn/ks/src/main/java/com/nbmediation/sdk/mobileads/KSNative.java
+++ adapter_cn/ks/src/main/java/com/nbmediation/sdk/mobileads/KSNative.java
@@ -2,12 +2,9 @@ package com.nbmediation.sdk.mobileads;
 
 import android.app.Activity;
 import android.content.Context;
-import android.text.TextUtils;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 
 import com.kwad.sdk.api.KsAdSDK;
 import com.kwad.sdk.api.KsAdVideoPlayConfig;
@@ -19,67 +16,67 @@ import com.nbmediation.sdk.mediation.CustomNativeEvent;
 import com.nbmediation.sdk.mediation.MediationInfo;
 import com.nbmediation.sdk.nativead.AdInfo;
 import com.nbmediation.sdk.nativead.NativeAdView;
-
 import java.util.List;
 import java.util.Map;
 
-import static android.widget.ListPopupWindow.MATCH_PARENT;
-
 public class KSNative extends CustomNativeEvent {
     private static String TAG = "OM-KSNative: ";
-    private Context mContext;
     private KsFeedAd  ksNAd;
     private View mNativeView;
     private Activity  av;
     @Override
     public void loadAd(final Activity activity, Map<String, String> config) {
         super.loadAd(activity, config);
+        String e = "loadAd传入的参数错误.";
         Log.d(TAG, "getLoadManager check");
         if (!check(activity, config)) {
             return;
         }
-
         if (activity == null || activity.isFinishing()) {
+            onInsError("activity is null");
             return;
         }
-        if (!check(activity, config)) {
-            return;
-        };
-        //暂时获取后面加上保护
+
         String appkeyStr = config.get("AppKey");
+        String appID = null;
+        String appName = null;
+        boolean isDebug = false;
         String[] split = appkeyStr.split("\\|");
-//        String appID = split[0];
-        String appID ="525400018";
-        String appName = split[1];
-        boolean isDebug = Boolean.parseBoolean(split[2]);
-        //end
+        if (split.length > 0) {
+            appID = split[0];
+        }else {
+            onInsError("not input AppID.");
+            return;
+        }
+        if (split.length > 1) {
+            appName = split[1];
+        }
+
+        if (split.length > 2) {
+            try {
+                isDebug = Boolean.parseBoolean(split[2]);
+            } catch (Exception ignored) {
+            }
+        }
         av = activity;
         initSdk(activity, appID, appName, isDebug);
-        int[] size = getNativeSize(config);
-        requestAd(Long.parseLong(mInstancesKey), size[0], size[1]);
+        requestAd(Long.parseLong(mInstancesKey));
     }
 
     private void initSdk(final Context activity, String appId, String appName, boolean isDebug) {
         KsAdSDK.init(activity, new SdkConfig.Builder()
-                .appId(appId) // 测试aapId，请联系快⼿手平台申请正式AppId，必填
-                .appName(appName) // 测试appName，请填写您应⽤用的名称，⾮非必填
+                .appId(appId) // aapId，必填
+                .appName(appName) //appName，非必填
                 .showNotification(true) // 是否展示下载通知栏 .debug(true)
                 .debug(isDebug) // 是否开启sdk 调试⽇日志 可选
                 .build());
-
     }
     /**
      * 请求Feed默认模板广告数据
      */
-    private void requestAd(long posId, int width, int height) {
+    private void requestAd(long posId) {
         KsScene scene = new KsScene.Builder(posId)
-                .width(width)
-                .adNum(1).build(); // 此为测试posId，请联系快手平台申请正式posId
-//        if (null== KsAdSDK.getLoadManager()) {
-//            Log.d(TAG, "getLoadManager is null");
-//        }else {
-//            Log.d(TAG, "getLoadManager");
-//        }
+                .adNum(1).build();
         KsAdSDK.getLoadManager()
                 .loadConfigFeedAd(scene, new KsLoadManager.FeedAdListener() {
                     @Override
@@ -89,6 +86,7 @@ public class KSNative extends CustomNativeEvent {
                     @Override
                     public void onFeedAdLoad(List<KsFeedAd> adList) {
                         Log.d(TAG, "onFeedAdLoad");
+                        int k = 0;
                         for (KsFeedAd ksFeedAd : adList) {
                             if (ksFeedAd != null) {
                                 ksNAd = ksFeedAd;
@@ -112,11 +110,10 @@ public class KSNative extends CustomNativeEvent {
     }
 
 
-    private View getAdItemView() {
+    private View getAdView() {
         if (null == ksNAd) {
             return null;
         }
-
         // 设置监听
         ksNAd.setVideoSoundEnable(false);//视频播放是否，默认静音播放
         ksNAd.setAdInteractionListener(new KsFeedAd.AdInteractionListener() {
@@ -138,19 +135,17 @@ public class KSNative extends CustomNativeEvent {
                 }
             }
         });
-
         return ksNAd.getFeedView(av.getBaseContext());
     }
 
 
     @Override
     public void registerNativeView(NativeAdView nativeAdView) {
-        mNativeView = ksNAd.getFeedView(av.getBaseContext());
+        mNativeView = getAdView();
         if (null == mNativeView) return;
         if (nativeAdView.getMediaView() != null) {
             nativeAdView.getMediaView().removeAllViews();
             nativeAdView.getMediaView().addView(mNativeView);
-
         }
 
     }
@@ -162,6 +157,9 @@ public class KSNative extends CustomNativeEvent {
 
     @Override
     public void destroy(Activity activity) {
+         if (ksNAd != null) {
+             ksNAd = null;
+         }
 
     }
 

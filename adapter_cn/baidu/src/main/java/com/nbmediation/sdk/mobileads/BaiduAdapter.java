b3ac2cec8c4faf7bb69d3e020319d7756ffa041f
Mirinda <15967863@qq.com>
2020-12-02 18:51:01 +0800

    插屏代码完成

--- adapter_cn/baidu/src/main/java/com/nbmediation/sdk/mobileads/BaiduAdapter.java
+++ adapter_cn/baidu/src/main/java/com/nbmediation/sdk/mobileads/BaiduAdapter.java
@@ -9,21 +9,29 @@ import android.util.Log;
 import com.nbmediation.sdk.mediation.CustomAdsAdapter;
 import com.nbmediation.sdk.mediation.InterstitialAdCallback;
 import com.nbmediation.sdk.mediation.MediationInfo;
+import com.nbmediation.sdk.mediation.RewardedVideoCallback;
 import com.nbmediation.sdk.mobileads.plugin6.BuildConfig;
+import com.baidu.mobads.rewardvideo.RewardVideoAd;
 
 
 import com.baidu.mobads.AdSize;
 import com.baidu.mobads.InterstitialAd;
 import com.baidu.mobads.InterstitialAdListener;
 import com.nbmediation.sdk.utils.AdLog;
+import com.nbmediation.sdk.utils.constant.KeyConstants;
 
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-public class BaiduAdapter extends CustomAdsAdapter implements InterstitialAdListener{
+public class BaiduAdapter extends CustomAdsAdapter implements InterstitialAdListener,RewardVideoAd.RewardVideoAdListener{
 
     private InterstitialAd mInterAd;            // 插屏广告实例，支持单例模式
     private InterstitialAdCallback mCallback;
     private static String TAG = "Baidu-Interstitial: ";
+    private static String TAGR = "Baidu-RewardedVideo: ";
+    private RewardedVideoCallback mRVCallback;
+    private AtomicBoolean isPreload = new AtomicBoolean();
+    public RewardVideoAd mRewardVideoAd;
 
     @Override
     public String getMediationVersion() {
@@ -124,7 +132,6 @@ public class BaiduAdapter extends CustomAdsAdapter implements InterstitialAdList
             mCallback.onInterstitialAdClosed();
         }
     }
-
     @Override
     public void onAdFailed(String arg0) {
         AdLog.getSingleton().LogD(TAG + "loadInterstitialAd onError " + arg0);
@@ -148,5 +155,150 @@ public class BaiduAdapter extends CustomAdsAdapter implements InterstitialAdList
             mCallback.onInterstitialAdLoadSuccess();
         }
     }
+
+    //rewarded video------------------------------------------------------------------------------//
+
+    @Override
+    public void initRewardedVideo(Context activity, Map<String, Object> dataMap, RewardedVideoCallback callback) {
+        super.initRewardedVideo(activity, dataMap, callback);
+        String error = check(activity);
+        if (TextUtils.isEmpty(error)) {
+            mRVCallback = callback;
+            if (callback != null) {
+                AdLog.getSingleton().LogD(TAGR + "onRewardedVideoInitSuccess");
+                callback.onRewardedVideoInitSuccess();
+            }
+        }else {
+            if (callback != null) {
+                AdLog.getSingleton().LogD(TAGR + "onRewardedVideoInitFailed");
+                callback.onRewardedVideoInitFailed(error);
+            }
+        }
+
+    }
+
+    @Override
+    public void loadRewardedVideo(Context activity, String adUnitId, RewardedVideoCallback callback) {
+        super.loadRewardedVideo(activity, adUnitId, callback);
+        loadRvAd(activity, adUnitId, callback);
+
+    }
+
+    @Override
+    public void loadRewardedVideo(Context activity, String adUnitId, Map<String, Object> extras, RewardedVideoCallback callback) {
+        super.loadRewardedVideo(activity, adUnitId, extras, callback);
+        loadRvAd(activity, adUnitId, callback);
+    }
+
+    private void loadRvAd(Context activity, String adUnitId, RewardedVideoCallback callback) {
+        AdLog.getSingleton().LogD(TAGR + "loadRvAd");
+        String error = check(activity, adUnitId);
+        if (TextUtils.isEmpty(error)) {
+            if (!isPreload.compareAndSet(false, true)) {
+                AdLog.getSingleton().LogD(TAGR + "ad loading, no need to load repeatedly");
+                callback.onRewardedVideoLoadFailed(TAGR + "ad loading, no need to load repeatedly");
+                return;
+            }
+
+            mRewardVideoAd = new RewardVideoAd(activity, adUnitId, BaiduAdapter.this, true);
+            if (null != mRewardVideoAd){
+                mRewardVideoAd.load();
+            }
+        } else {
+            if (callback != null) {
+                AdLog.getSingleton().LogD(TAGR + "null !=  TextUtils.isEmpty(error)");
+                callback.onRewardedVideoLoadFailed(error);
+            }
+        }
+    }
+
+    @Override
+    public void showRewardedVideo(Context activity, String adUnitId, RewardedVideoCallback callback) {
+        super.showRewardedVideo(activity, adUnitId, callback);
+        String error = check(activity, adUnitId);
+        if (!TextUtils.isEmpty(error)) {
+            if (callback != null) {
+                AdLog.getSingleton().LogD(TAGR + "onRewardedVideoAdShowFailed");
+                callback.onRewardedVideoAdShowFailed(error);
+            }
+            return;
+        }
+        if (isRewardedVideoAvailable(adUnitId)) {
+            isPreload.set(false);
+            if (null != mRewardVideoAd) {
+                mRewardVideoAd.show();
+            }
+        }else {
+            if (callback != null) {
+                AdLog.getSingleton().LogD(TAGR + "RewardedVideo is not ready");
+                callback.onRewardedVideoAdShowFailed(TAGR + "RewardedVideo is not ready");
+            }
+        }
+    }
+
+
+    @Override
+    public boolean isRewardedVideoAvailable(String adUnitId) {
+        if (TextUtils.isEmpty(adUnitId)) {
+            return false;
+        }
+        return  mRewardVideoAd != null && mRewardVideoAd.isReady();
+
+    }
+
+    @Override
+    public void onAdShow() {
+        if (mRVCallback != null) {
+            AdLog.getSingleton().LogD(TAGR + "onAdShow");
+            mRVCallback.onRewardedVideoAdShowSuccess();
+            AdLog.getSingleton().LogD(TAGR + "onAdStarted");
+            mRVCallback.onRewardedVideoAdStarted();
+        }
+    }
+
+    @Override
+    public void onAdClick() {
+        if (mRVCallback != null) {
+            AdLog.getSingleton().LogD(TAGR + "onAdClick");
+            mRVCallback.onRewardedVideoAdClicked();
+        }
+
+    }
+
+    @Override
+    public void onAdClose(float v) {
+        if (mRVCallback != null) {
+            AdLog.getSingleton().LogD(TAGR + "onAdClick");
+            mRVCallback.onRewardedVideoAdClosed();
+        }
+    }
+
+
+
+    @Override
+    public void onVideoDownloadSuccess() {
+        AdLog.getSingleton().LogD(TAGR + "onVideoDownloadSuccess");
+        if (mRVCallback != null) {
+            mRVCallback.onRewardedVideoLoadSuccess();
+        }
+    }
+
+    @Override
+    public void onVideoDownloadFailed() {
+        AdLog.getSingleton().LogD(TAGR + "onVideoDownloadFailed");
+        if (mRVCallback != null) {
+            mRVCallback.onRewardedVideoLoadFailed("get RewardedVideo error.");
+        }
+    }
+
+    @Override
+    public void playCompletion() {
+        AdLog.getSingleton().LogD(TAGR + "playCompletion");
+        if (mRVCallback != null) {
+            mRVCallback.onRewardedVideoAdEnded();
+        }
+    }
+
+
 
 }
